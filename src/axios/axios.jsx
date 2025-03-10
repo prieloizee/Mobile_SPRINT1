4392932f3d453a528ae389fc1683a2870de11a11
Malu1313 <marialuisavinholireis@gmail.com>
2025-03-10 15:32:06 -0300

    10-03

---
+++ src/axios/axios.jsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL:"http://10.89.240.85:5000/api/v1/",
+    headers:{
+        'accept':'application/json'
+    }
+});
+
+const sheets = {
+    postLogin:(user)=>api.post("login", user),
+    postUser:(user)=>api.post("user", user)
+}
+export default sheets;
